13932160a0dca12150f0ca9f0c21b6ca5362a0e9
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-10 21:22:34 -0300

    refactor: updating config to only use environment variables

--- Backend/src/database/config/config.js
+++ Backend/src/database/config/config.js
@@ -3,10 +3,11 @@ require('dotenv').config({ path: __dirname+'/./../../../.env' });
 module.exports = {
     development: {
         username: process.env.DATABASE_USER,
-        password: process.env.PASSWORD,
+        password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE,
-        host: '127.0.0.1',
+        host: process.env.DATABASE_HOST,
         dialect: 'mysql',
+        port: process.env.DATABASE_PORT,
         logging: false,
         define: {
             timestamps: true
