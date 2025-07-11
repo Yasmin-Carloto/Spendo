c3ae9130f9a6f90fd061241518079b3eec1b3e5e
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-10 21:23:01 -0300

    feat: uniting routes in index for organizational pourposes

---
+++ Backend/src/routes/index.js
@@ -0,0 +1,11 @@
+const userRoute = require("./user.route")
+const transactionRoute = require("./transaction.route")
+const categoryRoute = require("./category.route")
+const goalRoute = require("./goal.route")
+
+module.exports = {
+    userRoute,
+    transactionRoute,
+    categoryRoute,
+    goalRoute,
+}
