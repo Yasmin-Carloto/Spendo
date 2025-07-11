db184808d108d212b6438590bd68d971787ce1e7
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-10 21:21:27 -0300

    refactor: updating user controller's function form

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,75 +1,69 @@
-const userService = require('../services/user.service'); // Corrigido o nome
-const { validationResult } = require('express-validator');
-const createError = require('http-errors');
+const userService = require('../services/user.service')
+const { validationResult } = require('express-validator')
+const createError = require('http-errors')
 
-const create = async function(req, res, next) {
+async function create(req, res, next) {
     try {
-        const errors = validationResult(req);
+        const errors = validationResult(req)
 
-        if (!errors.isEmpty()) {
-            throw createError(422, { errors: errors.array() });
-        }
+        if (!errors.isEmpty()) throw createError(422, { errors: errors.array() })
 
-        const response = await userService.create(req.body);
-        if (response && response.message) {
-            throw response;
-        }
-        res.send(response);
+        const response = await userService.create(req.body)
+        res
+            .status(201)
+            .send(response)
     } catch (error) {
-        return next(error);
+        return next(error)
     }
-};
+}
 
-const login = async function(req, res, next) {
+async function login(req, res, next) {
     try {
-        const errors = validationResult(req);
+        const errors = validationResult(req)
 
-        if (!errors.isEmpty()) {
-            throw createError(422, { errors: errors.array() });
-        }
+        if (!errors.isEmpty()) throw createError(422, { errors: errors.array() })
 
-        const response = await userService.login(req.body);
-        if (response && response.message) {
-            throw response;
-        }
-        res.send(response);
+        const response = await userService.login(req.body)
+        res
+            .status(200)
+            .send(response)
     } catch (error) {
-        return next(error);
+        return next(error)
     }
-};
+}
 
-const encontrarTodos = async function(req, res, next) {
+async function getUserInfo(req, res, next) {
     try {
-        const response = await userService.encontrarTodos();
-        res.send(response);
+        const response = await userService.getUserInfoById(req.user.id)
+        res
+            .status(200)
+            .send(response)
     } catch (error) {
-        next(error);
+        next(error)
     }
-};
+}
 
-const encontrarPorId = async function(req, res, next) {
-    try {
-        const errors = validationResult(req);
+async function update(req, res, next) {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) throw createError(422, { errors: errors.array() })
 
-        if (!errors.isEmpty()) {
-            throw createError(422, { errors: errors.array() });
-        }
+    const user = await userService.update(req.body, req.user.id)
+    res
+        .status(200)
+        .send({
+            message: "User updated successfully",
+            user
+        })
+  } catch (error) {
+    next(error)
+  }
+}
 
-        const response = await userService.encontrarPorId(req.params.id);
-
-        if (response && response.message) {
-            throw response;
-        }
-
-        res.send(response);
-    } catch (error) {
-        next(error);
-    }
-};
 
 module.exports = {
     create,
-    encontrarTodos,
-    encontrarPorId,
+    getUserInfo,
     login,
-};
+    update
+}
