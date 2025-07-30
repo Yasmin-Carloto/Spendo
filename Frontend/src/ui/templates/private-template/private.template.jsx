2ef3b13d61ebdd5a2d689f2042e6239964ce08e7
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-29 22:34:26 -0300

    update: private template to use side bars

--- Frontend/src/ui/templates/private-template/private.template.jsx
+++ Frontend/src/ui/templates/private-template/private.template.jsx
@@ -1,10 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"
+import FixedSidebar from "@/ui/components/side-bar/fixed-side-bar"
+import spendoLogo from "@/ui/assets/spendo-logo.svg"
+import {
+  SidebarProvider
+} from "@/components/ui/sidebar"
+import FlexibleSidebar from "@/ui/components/side-bar/flexible-side-bar"
 
 export default function PrivateTemplate() {
   return (
-    <div>
-      <h1>Vai ter em todos, menos os de login e etc</h1>
-      <Outlet />
+    <div className="flex w-screen h-screen overflow-hidden relative">
+      <SidebarProvider>
+        <div className="hidden md:flex">
+          <FixedSidebar />
+        </div>
+
+        <div className="md:hidden absolute top-0 left-0 z-50 flex items-center gap-4 bg-white w-full h-16 px-4 shadow-sm">
+          <FlexibleSidebar />
+          <img src={spendoLogo} className="h-8 w-auto" />
+        </div>
+
+        <main className="flex-1 overflow-auto pt-16 md:pt-0">
+          <Outlet />
+        </main>
+      </SidebarProvider>
     </div>
   )
 }
