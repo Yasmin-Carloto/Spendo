5e7cd17f7ba32cbc77b0ac5a9beacb8bb749dab0
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-30 22:11:25 -0300

    feat: adding /habits route

--- Frontend/src/ui/utils/sidebar-items.js
+++ Frontend/src/ui/utils/sidebar-items.js
@@ -5,42 +5,48 @@ import {
   Clock, 
   HelpCircle, 
   Info, 
-  User 
+  User,
+  ShoppingBag
 } from "lucide-react"
 export default [
-    {
-      title: "Dashboard",
-      url: "/",
-      icon: LayoutDashboard,
-    },
-    {
-      title: "Transações",
-      url: "/transactions",
-      icon: ListOrdered,
-    },
-    {
-      title: "Metas",
-      url: "goals",
-      icon: Target,
-    },
-    {
-      title: "Histórico",
-      url: "/historic",
-      icon: Clock,
-    },
-    {
-      title: "FAQ",
-      url: "/faq",
-      icon: HelpCircle,
-    },
-    {
-      title: "Sobre Nós",
-      url: "/about-us",
-      icon: Info,
-    },
-    {
-      title: "Perfil",
-      url: "/profile",
-      icon: User,
-    },
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Transações",
+    url: "/transactions",
+    icon: ListOrdered,
+  },
+  {
+    title: "Metas",
+    url: "/goals",
+    icon: Target,
+  },
+  {
+    title: "Histórico",
+    url: "/historic",
+    icon: Clock,
+  },
+  {
+    title: "Hábitos de consumo",
+    url: "/habits",
+    icon: ShoppingBag,
+  },
+  {
+    title: "FAQ",
+    url: "/faq",
+    icon: HelpCircle,
+  },
+  {
+    title: "Sobre Nós",
+    url: "/about-us",
+    icon: Info,
+  },
+  {
+    title: "Perfil",
+    url: "/profile",
+    icon: User,
+  },
 ]
