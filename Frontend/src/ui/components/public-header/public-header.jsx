1eaa273cbf342f09aa15616cb7154ff0b6b3d0bb
Yasmin-Carloto <yasmincarlotobezerra17@gmail.com>
2025-07-30 22:11:10 -0300

    fix: logo appearence

--- Frontend/src/ui/components/public-header/public-header.jsx
+++ Frontend/src/ui/components/public-header/public-header.jsx
@@ -2,11 +2,8 @@ import spendoLogo from "@/ui/assets/spendo-logo.svg"
 
 export function PublicHeader() {
     return (
-        <header className="p-6 sticky top-0 z-10">
-            <img 
-                className="h-8 w-auto"
-                src={spendoLogo}
-            />
+        <header className="pt-8 pl-8">
+            <img src={spendoLogo} />
         </header>
     )
 }
